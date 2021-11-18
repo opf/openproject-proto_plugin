745a3b4952f0a3213baf7e9604d675cf50521d8a
Peter Nguyen <peter@bitcrowd.net>
2021-11-18 11:11:53 +0700

    add icon and handler

--- frontend/module/context-menu.ts
+++ frontend/module/context-menu.ts
@@ -1,22 +1,31 @@
-import {
-  WorkPackageAction,
-} from 'core-app/features/work-packages/components/wp-table/context-menu-helper/wp-context-menu-helper.service';
+import { WorkPackageAction } from "core-app/features/work-packages/components/wp-table/context-menu-helper/wp-context-menu-helper.service";
+import { WorkPackageResource } from "core-app/features/hal/resources/work-package-resource";
 
-const action:WorkPackageAction = {
-    text: 'Create Kittens',
-    key: 'newactionkey',
-    link: '/angular_kittens',
-}
+/*  
+  USAGE: import kittenAction into OpenProject core in thw wp-context-menu-directive file
+  in the buildItems function edit the following line to include the plugins actions
+  
+  const items = this.permittedActions.map((action:WorkPackageAction) => ({
+  
+  to
 
-export const menuItem = {
-  class: undefined as string|undefined,
-  disabled: false,
-  linkText: action.text,
-  href: action.href,
-  icon: action.icon != null ? action.icon : `icon-${action.key}`,
-  onClick: (_:JQuery.TriggeredEvent) => {
-    // do something
-    window.location.href=action.link!
-    return true;
-  },
-}
+  const items = this.permittedActions.concat([kittenAction]).map((action:WorkPackageAction) => ({
+*/
+
+type WorkPackagesHandler = (workPackages: WorkPackageResource[]) => void;
+
+// TODO handle not logged in
+// icons are located in /vendor/openproject-icon-font/src/icon.svg
+// if no icon is defined, then the key name is used to match an icon.
+export const kittenAction: WorkPackageAction = {
+  text: "Create Kittens",
+  key: "createkittens",
+  link: "/angular_kittens",
+  href: undefined,
+  icon: 'icon-info1',
+  indexBy: undefined,
+};
+
+export const kittenActionHandler: WorkPackagesHandler = (workPackages: WorkPackageResource[]) => {
+  window.location.href = kittenAction.link + '?ids=' + workPackages.map(wp => wp.id).join(',');
+};
