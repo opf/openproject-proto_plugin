833cbbd45d96333c6c4e9b71f0240b54c6d326f5
Martin Czuchra <martin@bitcrowd.net>
2021-11-24 09:55:03 +0100

    chown also before bundle install

--- docker/ci/entrypoint.sh
+++ docker/ci/entrypoint.sh
@@ -51,6 +51,7 @@ if [ "$1" == "setup-tests" ]; then
 		chmod 1777 "$folder"
 	done
 
+	execute "chown -hR dev /usr/local/bundle"
 	execute "time bundle install -j$JOBS"
 	execute "chown -hR dev /usr/local/bundle"
 	execute "TEST_ENV_NUMBER=0 time bundle exec rake db:create db:migrate db:schema:dump webdrivers:chromedriver:update webdrivers:geckodriver:update openproject:plugins:register_frontend"
